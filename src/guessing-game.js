c67f197b7cd8fb33b5701ff2c515d64b1365f700
A-nastasi-a <evsihneyeva@gmail.com>
2022-08-01 14:39:08 +0400

    feat: add solvation of the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.numbers = [];
+        this.number;
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.numbers = [];
+        for (let i = this.min; i < this.max; i++) {
+            this.numbers.push(i);
+        }
+        return this.numbers;
     }
 
     guess() {
-
+        this.number = this.numbers[Math.round(this.numbers.length / 2)];
+        return this.number;
     }
 
     lower() {
-
+        this.setRange(this.min, this.number);
     }
 
     greater() {
-
+        this.setRange(this.number, this.max);
     }
 }
 
